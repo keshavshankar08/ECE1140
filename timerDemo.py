2d35cf84aead1d73d013fe3f31e92bbc03dea0a2
Alex Ivensky <awi4@pitt.edu>
2023-10-24 18:51:30 -0400

    created simple demo of system clock

---
+++ timerDemo.py
@@ -0,0 +1,18 @@
+from PyQt6.QtCore import QThread, QCoreApplication
+from signals import signals
+from SystemTime import SystemTime
+import sys
+
+# run this function to see how the clock works
+
+def dummyFunction(current_time):
+        print("Current Time:", current_time)
+
+if __name__ == '__main__':
+        app = QCoreApplication([])
+        signals.current_system_time.connect(dummyFunction)
+        thread = QThread() 
+        system_time = SystemTime()
+        system_time.moveToThread(thread) 
+        thread.start()
+        sys.exit(app.exec())
