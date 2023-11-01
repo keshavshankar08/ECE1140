aeb7e4fb46b2bd5211617c79a5afa9494decbf07
Alex Ivensky <awi4@pitt.edu>
2023-11-01 16:54:54 -0400

    #60 added signal

--- signals.py
+++ signals.py
@@ -6,6 +6,7 @@ class signalsList(QObject):
     current_system_time = pyqtSignal(QTime)
     stop_timer = pyqtSignal()
     main_backend_update_values = pyqtSignal()
+    main_backend_update_track = pyqtSignal(Track)
 
     # SW Wayside Signals
     sw_wayside_track_update = pyqtSignal(Track)
@@ -13,6 +14,9 @@ class signalsList(QObject):
     # Train Model Signals
     trainModel_test_changeSpeed = pyqtSignal(float)
     trainModel_send_actual_velocity = pyqtSignal(float)
+    
+    # Train Controller Signals
+    trainController_send_power_command = pyqtSignal(float)
 
 signals = signalsList()
 
