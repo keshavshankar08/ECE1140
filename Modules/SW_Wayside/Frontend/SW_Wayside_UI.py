4470c32143340476a9f89234b4cf6cd1609d7cc0
Keshav Shankar <keshavshankar08@gmail.com>
2023-11-08 01:51:17 -0500

    #39 added all necessary functions and functionality for ui

--- Modules/SW_Wayside/Frontend/SW_Wayside_UI.py
+++ Modules/SW_Wayside/Frontend/SW_Wayside_UI.py
@@ -11,68 +11,203 @@ class SWWaysideFrontend(QtWidgets.QMainWindow):
         def __init__(self):
                 super().__init__()
                 uic.loadUi("Modules/SW_Wayside/Frontend/SW_Wayside_UI.ui", self)
-                self.trackInstanceCopy = Track()
+                self.track_instance_copy = Track()
 
-                # Signal connections
-                signals.sw_wayside_frontend_update.connect(self.update_frontend)
+                # Receives updates from backend
+                signals.sw_wayside_update_frontend.connect(self.update_frontend)
+
+                # If any change in manual mode, need to override system with their inputs
+                self.switch_direction_dropdown.currentTextChanged.connect(self.manual_switch_toggled)
+                self.traffic_light_color_dropdown.currentTextChanged.connect(self.manual_light_toggled)
+                self.crossing_status_dropdown.currentTextChanged.connect(self.manual_crossing_toggled)
+
+                # If PLC upload button clicked
+                self.upload_plc_program_button.clicked.connect(self.uploadPLCClicked)
+
+                # If view track map button clicked
+                self.track_map_view_button.clicked.connect(self.view_track_map_clicked)
 
                 self.show()
         
-        # Main function to call all updates to UI
-        def update_frontend(self, trackInstance):
-                pass
+        # Handles all frontend updates
+        def update_frontend(self, track_instance):
+                # update local instance of track
+                self.track_instance_copy = track_instance
 
-        # Main function to send all updates to backend
-        def frontend_update(self, trackInstance):
-                pass
+                # update the ui information
+                self.update_display()
+
+                # send any updates back to backend
+                self.frontend_update()
+
+        # Sends updates to backend
+        def frontend_update(self):
+                signals.sw_wayside_frontend_update.emit(self.track_instance_copy)
+
+        # Calls all UI information
+        def update_display(self):
+                self.update_opeartion_dropdown()
+                self.update_line_dropdown()
+                self.update_wayside_dropdown()
+                self.update_block_dropdown()
+                self.update_block_information()
+
+        # Updates copy track instance
+        def update_copy_track(self, updatedTrack):
+                self.trackInstanceCopy = updatedTrack
         
-        # Updates display based on operation mode
+        # Updates elements shown once mode chosen
         def update_opeartion_dropdown(self):
-                pass
+                if(self.mode_selection_dropdown.currentText() == "Select Mode..."):
+                        self.mode_selection_dropdown.setEnabled(False)
+                        self.line_box.setEnabled(False)
+                        self.switch_direction_dropdown.setEnabled(False)
+                        self.traffic_light_color_dropdown.setEnabled(False)
+                        self.crossing_status_dropdown.setEnabled(False)
+                elif(self.mode_selection_dropdown.currentText() == "Manual"):
+                        self.mode_selection_dropdown.setEnabled(False)
+                        self.line_box.setEnabled(True)
+                        self.switch_direction_dropdown.setEnabled(True)
+                        self.traffic_light_color_dropdown.setEnabled(True)
+                        self.crossing_status_dropdown.setEnabled(True)
+                elif(self.mode_selection_dropdown.currentText() == "Automatic"):
+                        self.mode_selection_dropdown.setEnabled(False)
+                        self.line_box.setEnabled(True)
+                        self.switch_direction_dropdown.setEnabled(False)
+                        self.traffic_light_color_dropdown.setEnabled(False)
+                        self.crossing_status_dropdown.setEnabled(False)
 
-        # Updates display based on line selection
+        # Updates elements shown once line chosen
         def update_line_dropdown(self):
-                pass
+                if(self.line_selection_dropdown.currentText() == "Select Line..."):
+                        self.track_map_view_button.setEnabled(False)
+                        self.wayside_box.setEnabled(False)
+                elif(self.line_selection_dropdown.currentText() == "Green Line"):
+                        self.track_map_view_button.setEnabled(True)
+                        self.wayside_box.setEnabled(True)
+                elif(self.line_selection_dropdown.currentText() == "Red Line"):
+                        self.track_map_view_button.setEnabled(True)
+                        self.wayside_box.setEnabled(True)
 
-        # Updates display based on wayside selection
+        # Updates elements shown once wayside chosen
         def update_wayside_dropdown(self):
-                pass
+                if(self.wayside_selection_dropdown.currentText() == "Select Wayside..."):
+                        self.upload_plc_program_button.setEnabled(False)
+                        self.block_box.setEnabled(False)
+                elif(self.wayside_selection_dropdown.currentText() == "Wayside 1"):
+                        self.upload_plc_program_button.setEnabled(True)
+                        self.block_box.setEnabled(True)
+                        self.block_selection_dropdown.clear()
+                        self.block_selection_dropdown.addItem("Select Block...")
+                        if(self.line_selection_dropdown.currentText() == "Green Line"):
+                                for i in range(69):
+                                        self.block_selection_dropdown.addItem("Block " + str(self.track_instance_copy.lines[1].blocks[i].blockNumber))
+                        elif(self.line_selection_dropdown.currentText() == "Red Line"):
+                                for i in range(24):
+                                        self.block_selection_dropdown.addItem("Block " + str(self.track_instance_copy.lines[0].blocks[i].blockNumber))
+                elif(self.wayside_selection_dropdown.currentText() == "Wayside 2"):
+                        self.upload_plc_program_button.setEnabled(True)
+                        self.block_box.setEnabled(True)
+                        self.block_selection_dropdown.clear()
+                        self.block_selection_dropdown.addItem("Select Block...")
+                        if(self.line_selection_dropdown.currentText() == "Green Line"):
+                                for i in range(69, 151):
+                                        self.block_selection_dropdown.addItem("Block " + str(self.track_instance_copy.lines[1].blocks[i].blockNumber))
+                        elif(self.line_selection_dropdown.currentText() == "Red Line"):
+                                for i in range(24, 77):
+                                        self.block_selection_dropdown.addItem("Block " + str(self.track_instance_copy.lines[0].blocks[i].blockNumber))
 
-        # Updates display based on block selection
+        # Updates elements shown once block chosen
         def update_block_dropdown(self):
-                pass
+                if(self.block_selection_dropdown.currentText() == "Select Block..."):
+                        self.general_box.setEnabled(False)
+                        self.maintenance_box.setEnabled(False)
+                        self.junction_box.setEnabled(False)
+                        self.station_box.setEnabled(False)
+                        self.crossing_box.setEnabled(False)
+                else:
+                        self.general_box.setEnabled(True)
+                        self.maintenance_box.setEnabled(True)
+                        curr_line_int = self.get_current_line_displayed_int()
+                        curr_block_int = self.get_current_block_displayed_int()
+                        if(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].blockType == "Junction"):
+                                self.junction_box.setEnabled(True)
+                                self.station_box.setEnabled(False)
+                                self.crossing_box.setEnabled(False)
+                                self.switch_direction_dropdown.clear()
+                                self.switch_direction_dropdown.addItems(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].get_switch_direction_string_list(curr_line_int))
+                                self.traffic_light_color_dropdown.clear()
+                                self.traffic_light_color_dropdown.addItems(["Red", "Green"])
+                        elif(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].blockType == "Station"):
+                                self.junction_box.setEnabled(False)
+                                self.station_box.setEnabled(True)
+                                self.crossing_box.setEnabled(False)
+                        elif(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].blockType == "Crossing"):
+                                self.junction_box.setEnabled(False)
+                                self.station_box.setEnabled(False)
+                                self.crossing_box.setEnabled(True)
+                                self.crossing_status_dropdown.clear()
+                                self.crossing_status_dropdown.addItems(["Active","Inactive"])
+                        else:
+                                self.crossing
 
-        # Updates display block dropdown options
-        def update_dropdown_blocks(self):
-                pass
-        
+        # Gets the integer representation of the current line chosen
+        def get_current_line_displayed_int(self):
+                curr_line = self.line_selection_dropdown.currentText()
+                if(curr_line == "Green Line"):
+                        return 1
+                elif(curr_line == "Red Line"):
+                        return 0
+                
+        # Gets the integer representation of the current block chosen
+        def get_current_block_displayed_int(self):
+                curr_block = self.block_selection_dropdown.currentText()
+                return int(curr_block[6:])
+                        
         # Updates display block information
         def update_block_information(self):
-                pass
+                curr_line_int = self.get_current_line_displayed_int()
+                curr_block_int = self.get_current_block_displayed_int()
+                self.block_type_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].block_type)
+                self.block_occupancy_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].block_occupancy)
+                self.track_fault_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].track_fault_status)
+                self.maintenance_active_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].maintenance_status)
+                self.switch_direction_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].switch_direction)
+                self.traffic_light_color_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].traffic_light_color)
+                self.station_name_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].station_name)
+                self.crossing_status_value.setText(self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].crossing_status)
 
         # Handles view track map button clicked
         def view_track_map_clicked(self):
-                pass
-        
-        # Handles switch toggle in manual mode
-        def manual_switch_toggled(self):
-                pass
-
-        # Handles light toggle in manual mode
-        def manual_light_toggled(self):
-                pass
-
-        # Handles crossing toggle in manual mode
-        def manual_crossing_toggled(self):
+                # figure out alternative to opencv to open up the map pictures
+                # need to make a good map with devices to show
                 pass
 
         # Handles upload plc program button clicked
         def uploadPLCClicked(self):
                 fileName = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt)")
+                # this will get fed into the interpreter
+                # the interpreter will spit back a wayside logic object
+                # this gets added to a copy of the wayside controller object, which contains the logic and info from the track for each wayside
+                # most importantly, it has a wayside logic object for each controller
+        
+        # Handles switch toggle in manual mode
+        def manual_switch_toggled(self):
+                curr_line_int = self.get_current_line_displayed_int()
+                curr_block_int = self.get_current_block_displayed_int()
+                self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].switch_direction = self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].get_switch_direction_bool(self.switch_direction_dropdown.currentText(), curr_line_int)
+        
+        # Handles light toggle in manual mode
+        def manual_light_toggled(self):
+                curr_line_int = self.get_current_line_displayed_int()
+                curr_block_int = self.get_current_block_displayed_int()
+                self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].traffic_light_color = self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].get_traffic_light_color_bool(self.traffic_light_color_dropdown.currentText())
 
-        # Updates copy track instance
-        def update_copy_track(self, updatedTrack):
-                self.trackInstanceCopy = updatedTrack
+        # Handles crossing toggle in manual mode
+        def manual_crossing_toggled(self):
+                curr_line_int = self.get_current_line_displayed_int()
+                curr_block_int = self.get_current_block_displayed_int()
+                self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].crossing_status = self.track_instance_copy.lines[curr_line_int].blocks[curr_block_int].get_crossing_status_bool(self.crossing_status_dropdown.currentText())
 
 if __name__ == "__main__":
         app = QtWidgets.QApplication(sys.argv)
--- signals.py
+++ signals.py
@@ -8,9 +8,16 @@ class signalsList(QObject):
     main_backend_update_values = pyqtSignal()
 
     # SW Wayside Signals
-    sw_wayside_backend_update = pyqtSignal(Track)
-    sw_wayside_frontend_update = pyqtSignal(Track)
-    sw_wayside_track_update = pyqtSignal(Track)
+    sw_wayside_update_backend = pyqtSignal(Track) # update from main backend to sw wayside backend
+    sw_wayside_update_frontend = pyqtSignal(Track) # update from sw wayside backend to sw wayside frontend
+    sw_wayside_frontend_update = pyqtSignal(Track) # update from sw wayside frontend to sw wayside backend
+    sw_wayside_backend_update = pyqtSignal(Track) # update from sw wayside backend to main backend
+    '''
+    Main Backend    ->  SW Wayside Backend  ->  SW Wayside Frontend
+                                                        |
+                                                        ▼
+                        Main Backend    <-      SW Wayside Backend             
+    '''
     
 
     # Train Model Signals
