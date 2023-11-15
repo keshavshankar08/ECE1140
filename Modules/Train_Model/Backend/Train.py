1298af0c96bdbaf97105de0e29b47d03f7c5b966
Alex Ivensky <awi4@pitt.edu>
2023-11-15 00:37:48 -0500

    #98 minor changes

--- Modules/Train_Model/Backend/Train.py
+++ Modules/Train_Model/Backend/Train.py
@@ -4,6 +4,7 @@
 
 from PyQt6.QtCore import QObject, QDateTime, pyqtSignal
 import sys
+
 sys.path.append(".")
 from signals import signals
 from CONSTANTS import START_YEAR, START_MONTH, START_DAY, START_HOUR, START_MIN, START_SEC, TIME_DELTA
@@ -11,19 +12,19 @@ from CONSTANTS import START_YEAR, START_MONTH, START_DAY, START_HOUR, START_MIN,
 import math
 
 #### Useful Constants
-CAR_LENGTH = 32.2 # meters
-CAR_HEIGHT = 3.42 # meters
-CAR_WIDTH = 2.65 # meters
-CAR_WEIGHT_EMPTY = 37103.86 # kg
-CAR_WEIGHT_LOADED = 51437.37 # kg
-MAX_SPEED = 19.444 # m/s
-S_BRAKE_FORCE = 61716 # N
-E_BRAKE_FORCE = 140404 # N
-MAX_MOTOR_POWER = 120000 # Watts
-GRAVITY = 9.8 # m/s^2
-MAX_ENGINE_FORCE = 40000 # N
-FRICTION_COEFF = 0 # dimensionless
-    
+CAR_LENGTH = 32.2  # meters
+CAR_HEIGHT = 3.42  # meters
+CAR_WIDTH = 2.65  # meters
+CAR_WEIGHT_EMPTY = 37103.86  # kg
+CAR_WEIGHT_LOADED = 51437.37  # kg
+MAX_SPEED = 19.444  # m/s
+S_BRAKE_FORCE = 61716  # N
+E_BRAKE_FORCE = 140404  # N
+MAX_MOTOR_POWER = 120000  # Watts
+GRAVITY = 9.8  # m/s^2
+MAX_ENGINE_FORCE = 40000  # N
+FRICTION_COEFF = 0  # dimensionless
+
 
 class Train(QObject):
     def __init__(self):
@@ -50,47 +51,47 @@ class Train(QObject):
         #### Number of Passengers
         self.numPassengers = 0
         #### Intrinsic Properties
-        self.mass = CAR_WEIGHT_EMPTY # kg
-        self.length = CAR_LENGTH # m
-        self.height = CAR_HEIGHT # m
-        self.width = CAR_WIDTH # m
+        self.mass = CAR_WEIGHT_EMPTY  # kg
+        self.length = CAR_LENGTH  # m
+        self.height = CAR_HEIGHT  # m
+        self.width = CAR_WIDTH  # m
         #### Doors - False is open, True is closed
-        self.leftDoor = False 
+        self.leftDoor = False
         self.rightDoor = False
         #### Lights - False is off, True is on
         self.interiorLight = False
         self.exteriorLight = False
         #### Interior Train Temperature
-        self.temperatureCommand = 60.0 # F
-        self.temperatureActual = 60.0 # F
+        self.temperatureCommand = 60.0  # F
+        self.temperatureActual = 60.0  # F
         #### Emergency Brake - either on or off
         self.emergencyBrake = False
         #### Service Brake - either on or off
         self.serviceBrake = False
         #### Instantaneous Values
         ## Speed
-        self.currentSpeed = 0.0 # m/s
-        self.previousSpeed = 0.0 # m/s
-        self.currentAccel = 0.0 # m/s^2
-        self.previousAccel = 0.0 # m/s^2
-        self.accelSum = 0.0 # m/s^2 ??
+        self.currentSpeed = 0.0  # m/s
+        self.previousSpeed = 0.0  # m/s
+        self.currentAccel = 0.0  # m/s^2
+        self.previousAccel = 0.0  # m/s^2
+        self.accelSum = 0.0  # m/s^2 ??
         ## Force
-        self.commandedPower = 0.0 # N*m / s
-        self.engineForce = 0.0 # N
-        self.slopeForce = 0.0 # N
-        self.netForce = 0.0 # N
-        self.brakeForce = 0.0 # N
-        self.frictionForce = 0.0 # N
+        self.commandedPower = 0.0  # N*m / s
+        self.engineForce = 0.0  # N
+        self.slopeForce = 0.0  # N
+        self.netForce = 0.0  # N
+        self.brakeForce = 0.0  # N
+        self.frictionForce = 0.0  # N
         ## Track
-        self.currentAuthority = 0.0 # m
-        self.currentBlock = 0 # dimensionless
-        self.trackPolarity = 1 # or -1
-        self.currentGradient = 0.0 # %
-        self.currentAngle = 0.0 # radians
+        self.currentAuthority = 0.0  # m
+        self.currentBlock = 0  # dimensionless
+        self.trackPolarity = 1  # or -1
+        self.currentGradient = 0.0  # %
+        self.currentAngle = 0.0  # radians
         self.distanceFromYard = 0.0
         self.distanceFromBlockStart = 0.0
         self.currentBeacon = None
-        self.beaconList = [] # list of all beacons received
+        self.beaconList = []  # list of all beacons received
         #### Time
         self.current_time = QDateTime(START_YEAR, START_MONTH, START_MONTH, START_DAY, START_HOUR, START_MIN, START_SEC)
         #### Failure
@@ -102,10 +103,9 @@ class Train(QObject):
         #### Passthroughs
         self.speedLimit = 70.0
         self.currentTime = QDateTime()
-        
+
     def updateCurrentTime(self, value):
         self.currentTime = value
-        
 
     def TrainModelUpdateValues(self):
         self.previousAccel = self.currentAccel
@@ -122,32 +122,30 @@ class Train(QObject):
             self.engineForce = abs(self.commandedPower / self.currentSpeed)
         except ZeroDivisionError:
             if (self.commandedPower > 0):
-                self.currentSpeed = 0.1 # ????
+                self.currentSpeed = 0.1  # ????
         ### SLOPE
         self.currentAngle = math.atan(self.currentGradient / 100)
         self.slopeForce = self.mass * GRAVITY * math.sin(self.currentAngle)
         ### FRICTION
         self.frictionForce = self.mass * GRAVITY * FRICTION_COEFF * math.cos(self.currentAngle)
-        
-        
+
         self.netForce = self.engineForce - self.slopeForce - self.brakeForce - self.frictionForce
         if (self.netForce > MAX_ENGINE_FORCE):
             self.netForce = MAX_ENGINE_FORCE
         self.currentAccel = self.netForce / self.mass
-        
+
         if (self.commandedPower <= MAX_MOTOR_POWER):
             self.currentSpeed = self.currentSpeed + (TIME_DELTA * 0.001 / 2) * (self.currentAccel + self.previousAccel)
-            
+
         if (self.currentSpeed < 0):
             self.currentSpeed = 0
-            
+
         if (self.currentSpeed > MAX_SPEED):
             self.currentSpeed = MAX_SPEED
-            
+
         if (self.currentSpeed > self.speedLimit):
             self.currentSpeed = self.speedLimit
-        
-            
+
         # Signals to Train Controller
         signals.trainModel_send_engine_failure.emit(self.engineFail)
         signals.trainModel_send_signal_failure.emit(self.signalFail)
@@ -161,10 +159,10 @@ class Train(QObject):
 
     def onEmergencyBrake(self):
         self.emergencyBrake = True
-        
+
     def offEmergencyBrake(self):
         self.emergencyBrake = False
-    
+
     def serviceBrakeReceive(self, value):
         if (value == 1):
             self.serviceBrake = True
@@ -173,49 +171,44 @@ class Train(QObject):
 
     def onInteriorLights(self):
         self.interiorLight = True
-    
+
     def offInteriorLights(self):
         self.interiorLight = False
 
     def onExteriorLights(self):
         self.exteriorLight = True
-    
+
     def offExteriorLights(self):
         self.exteriorLight = False
 
     def openLeftDoors(self):
         self.leftDoor = False
-    
+
     def closeLeftDoors(self):
         self.leftDoor = True
 
     def openRightDoors(self):
         self.rightDoor = False
-    
+
     def closeRightDoors(self):
         self.rightDoor = True
 
     def receiveTemperature(self, value):
         self.temperatureCommand = value
-    
+
     def setCurrentTime(self, time):
         self.current_time = time
-        
+
     def setPowerCommand(self, value):
         self.commandedPower = value
-        
+
     def receiveBeacon(self, beacon):
         self.beaconList.append(beacon)
-    
+
     def showAdvertisement(self):
         pass
-    
-    
+
+
 trains = {
     1: Train()
 }
-        
-
-    
-
-
