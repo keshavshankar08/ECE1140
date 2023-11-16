b7212db858789cf643fe2bd2b28f39b5a1055e8f
John Deibert <johndeib121@gmail.com>
2023-11-15 20:20:52 -0500

    merged from backend

--- Modules/Train_Controller/Backend/Train_Controller.py
+++ Modules/Train_Controller/Backend/Train_Controller.py
@@ -3,7 +3,6 @@ import sys
 from PyQt6.QtCore import QObject, QDateTime, pyqtSignal
 sys.path.append(".")
 from signals import signals
-import threading
 from CONSTANTS import START_YEAR, START_MONTH, START_DAY, START_HOUR, START_MIN, START_SEC, TIME_DELTA
 
 #This class represents the train controller
