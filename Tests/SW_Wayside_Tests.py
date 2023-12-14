827b2b3e033a5fe78fff0dc85e1658dfe3ee2a5b
Keshav Shankar <keshavshankar08@gmail.com>
2023-12-14 10:09:55 -0500

    #231 declared test cases

--- Tests/SW_Wayside_Tests.py
+++ Tests/SW_Wayside_Tests.py
@@ -6,6 +6,7 @@ from Main_Backend import *
 from Track_Resources.PLC import *
 
 class automatic_mode_tests(unittest.TestCase):
+    # ----- PLC load tests -----
     def test_plc_file_uploaded_updates_tokens(self):
         # Arrange
         plc = PLC()
@@ -15,6 +16,41 @@ class automatic_mode_tests(unittest.TestCase):
 
         # Assert
         self.assertTrue(len(plc.green_line_wayside1_token_list) > 0)
+
+    def test_plc_file_interpreted(self):
+        # Arrange
+        plc = PLC()
+    
+    # ----- Signal tests -----
+    # signals from ctc
+    def test_wayside_receives_authority(self):
+        pass
+
+    def test_wayside_receives_suggested_speed(self):
+        pass
+
+    def test_wayside_receives_maintenance(self):
+        pass
+    
+    # signals from track model
+    def test_wayside_receives_track_occupancy(self):
+        pass
+
+    def test_wayside_receives_track_fault(self):
+        pass
+
+    # ---- PLC logic tests -----
+    def test_wayside_changes_traffic_light(self):
+        pass
+    
+    def test_wayside_changes_switch_direction(self):
+        pass
+
+    def test_wayside_changes_crossing_active(self):
+        pass
+
+    def test_wayside_changes_authority(self):
+        pass
 
 class manual_mode_tests(unittest.TestCase):
     pass
